ece8738530b18928663b80f62617a3d4e1d705b1
binLearning <binlearning@163.com>
2017-07-24 12:02:36 +0800

    Create run_1node_example.sh

---
+++ test_intel_knl/run_1node_example.sh
@@ -0,0 +1,1 @@
+mpirun -l -n 1 -ppn 1 -machinefile mpd_1nodes.hosts -genv PSM2_MQ_RNDV_HFI_WINDOW=2097152 -genv OMP_NUM_THREADS=56 -genv KMP_AFFINITY="proclist=[0-55],granularity=thread,explicit" -genv KMP_HW_SUBSET=1t /home/test/intelcaffe/build/tools/caffe train --solver=solver.prototxt --engine=MKL2017 > ./log/log_1node.txt 2>&1 &
